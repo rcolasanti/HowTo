fd692c96a64bd73dc056ae55037a441742e84aa1
Ric <developer.colasanti@gmail.com>
2018-01-04 20:57:11 +0000

    d3 example with simpler data

--- D3/main.js
+++ D3/main.js
@@ -1,40 +1,34 @@
-var dataset
+var dataset = ["A", "B", "C", "D", "E", "F", "G"]
 
-d3.json("data.json",function(data){
-        dataset = data
-        update()
-    }
-);
+function update() {
+  ///selects all of the paragraphs that are currently on the page
+  var currentTxt = d3.select("#dataArea").selectAll("p")
+    .data(dataset)
 
-function update(){
-        ///selects all of the paragraphs that are currently on the page
-        var currentTxt = d3.select("#dataArea").selectAll("p")
-        .data(dataset)
+  //update thoes data that are present
+  currentTxt.style("color", "rgba(0,0,200,1)")
+    .text(function(d) {
+      return d;
+    })
 
-        //update thoes data that are present
-        currentTxt.style("color","rgba(0,0,200,1)")
-        .text(function(d){
-            return d.id
-        })
+  // add new data
+  var newTxt = currentTxt.enter()
 
-        // add new data
-        var newTxt = currentTxt.enter()
-
-        newTxt.append("p")
-        .text(function(d){
-          return d.id
-        })
-        .on("click",function(d){
-            d.id="X"
-            update()
-        })
-        .style("color","rgba(200,100,0,1)")
-        .style("cursor", "pointer")
+  newTxt.append("p")
+    .text(function(d) {
+      return d
+    })
+    .on("click", function(d,i) {
+      dataset[i] = "X"
+      update()
+    })
+    .style("color", "rgba(200,100,0,1)")
+    .style("cursor", "pointer")
 }
 
-
-
-function addData(){
-    dataset.push({"id":"Z","data":11})
-    update()
+function addData() {
+  dataset.push("Z")
+  update()
 }
+
+update()
