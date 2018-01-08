552849660ed57f2e2155ad457681e125972cd1a7
Ric <developer.colasanti@gmail.com>
2018-01-08 19:42:31 +0000

    Add zoom and translate

--- D3/Tree/data.js
+++ D3/Tree/data.js
@@ -17,22 +17,6 @@ var data = {
 };
 
 
-function find(object,searchFor) {
-  if (object.hasOwnProperty('name') && object.name == searchFor){
-    return object;
-  }
-
-  if (object.hasOwnProperty('children')){
-    for (var i = 0; i < object.children.length; i++) {
-      var res = find(object.children[i],searchFor)
-      if (res !=null){
-        return res;
-      }
-    }
-  }
-  return null;
-}
-
 
 function addData(object,newName){
   if(object.hasOwnProperty('children')==false){
@@ -40,5 +24,3 @@ function addData(object,newName){
   }
   object.children.push({"name":newName})
 }
-
-console.log(find(data,"A-1-1"));
--- D3/Tree/main.js
+++ D3/Tree/main.js
@@ -3,16 +3,25 @@ var tree = d3.tree().size([300, 500]);
 // take the nested data and turn it into a d3 hierarchy. This has aditional hierarchy data like depth and parent
 
 //This crates the x and y values for each node of root in accordance with the tree layout.
+var root = d3.hierarchy(data);
+
+var zoom = d3.zoom().on("zoom", zoomed);
+
+function zoomed() {
+  var currentTransform = d3.event.transform;
+  svg.attr("transform", currentTransform);
+}
+
+var svg = d3.select("#svgCanvas").call(zoom)
 
 function update() {
-  var root = d3.hierarchy(data);
   var treeData = tree(root)
   var dNodes = treeData.descendants();
   var dLinks = treeData.links()
   dNodes.forEach(function(d) {
     d.y = d.depth * 130
   });
-  var currentLinks = d3.select("#svgcanvas").selectAll("path.link")
+  var currentLinks = d3.select("#svgArea").selectAll("path.link")
     .data(dLinks) // an array of all of the links of the tree
 
   var newLinks = currentLinks.enter();
@@ -29,13 +38,12 @@ function update() {
         y: d.target.y
       }
       path = diagonal(s, t);
-      console.log(path);
       return path;
     });
 
+  currentLinks.exit().remove()
 
-
-  var currentNodes = d3.select("#svgcanvas").selectAll("g.node")
+  var currentNodes = d3.select("#svgArea").selectAll("g.node")
     .data(dNodes) // an array of all of the nodes of the tree
 
 
@@ -44,12 +52,11 @@ function update() {
     .append('g')
     .attr("cursor", "pointer")
     .classed('node', true) //adds node class
-    .on("click", function(d) {
-      console.log("colapse");
-    })
+    .on("click", click)
     .on("contextmenu", function(d, i) {
       d3.event.preventDefault();
       addData(d.data, d.data.name + " new")
+      root = d3.hierarchy(data);
       update()
     })
 
@@ -65,8 +72,10 @@ function update() {
     return "translate(" + d.y + "," + d.x + ")";
   })
 
-}
+  currentNodes.exit().remove()
 
+
+}
 
 // Creates a curved  path from source to target nodes
 // THis is set for rotated tree
@@ -78,4 +87,16 @@ function diagonal(s, d) {
 
   return path
 }
+
+function click(d) {
+  if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+  update();
+}
+
 update()
